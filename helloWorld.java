2a2f15a1fd0d335816ff64f26a6aeaa2f344ee5d
Shavithak <141250070+Shavithak@users.noreply.github.com>
2023-08-10 14:58:10 +0530

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -1,8 +1,10 @@
+public class helloWorld {
+
 public static void main(String[] args)
-	{
-	String s1= "Hello WOrld"
-  String s2= "SDET training demo of free style project"  
-	System.out.println(S1);
-	System.out.println(S2);	
-		} 
-			
+{
+	String s1= "Hello World" ;
+        String s2= "SDET training demo of free style project"  ;
+	System.out.println(s1);
+	System.out.println(s2);	
+} 
+}

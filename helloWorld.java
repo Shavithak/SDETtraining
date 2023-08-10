2749578641d457ef36dbc3cee362a13ff88a33f1
Shavithak <141250070+Shavithak@users.noreply.github.com>
2023-08-10 14:52:57 +0530

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,8 @@
+public static void main(String[] args)
+	{
+	String s1= "Hello WOrld"
+  String s2= "SDET training demo of free style project"  
+	System.out.println(S1);
+	System.out.println(S2);	
+		} 
+			
